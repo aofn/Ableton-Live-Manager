4838ae91f83e52fef0dc63915207f503085de1e9
Marcel <79279756+aofn@users.noreply.github.com>
2025-01-23 23:14:06 +0100

    feat: update Tags component to check for Ableton 12 tags
    - Updated Tags component to check the `Ableton Folder Info` folder for tags
    - Added parseXmpFiles method in Ableton.js to handle reading and parsing .xmp files

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { writeTextFile } from "@tauri-apps/api/fs";
 import { Badge, badgeVariants } from "@/components/ui/badge";
@@ -12,6 +13,7 @@ import {
 import AddTagDialogue from "@/components/AddTagDialogue";
 import { Cross2Icon, PlusIcon } from "@radix-ui/react-icons";
 import { cn } from "@/lib/utils";
+import Ableton from "@/lib/Ableton";
 
 export const Tags = ({
   setAlmFile,
@@ -24,9 +26,18 @@ export const Tags = ({
   const availableTags = config.tags;
   const { t } = useTranslation();
   const copyAlmFile = { ...almFile };
+  const [xmpKeywords, setXmpKeywords] = useState([]);
+
+  useEffect(() => {
+    const fetchXmpKeywords = async () => {
+      const keywords = await Ableton.parseXmpFiles(projectDirectory);
+      setXmpKeywords(keywords);
+    };
+
+    fetchXmpKeywords();
+  }, [projectDirectory]);
 
   const writeTagToAlmFile = async (tag) => {
-    // grab string from last slash (project file name)
     function addOrUpdateObjectWithTag(mainObject, addObject, key) {
       if (!mainObject.hasOwnProperty(key)) {
         mainObject[key] = {};
@@ -40,13 +51,13 @@ export const Tags = ({
 
     addOrUpdateObjectWithTag(copyAlmFile, tag, "tags");
 
-    // otherwise we save the changes
     setAlmFile(copyAlmFile);
     return writeTextFile(
       `${projectDirectory}/alm.json`,
       JSON.stringify(copyAlmFile, null, 2),
     );
   };
+
   const removeTag = async (key) => {
     const copyAlmFile = { ...almFile };
     delete almFile.tags[key];
@@ -60,66 +71,58 @@ export const Tags = ({
   return (
     <div className="flex flex-row">
       {almFile?.tags &&
-        _.map(almFile.tags, (tag, key) => {
-          return (
-            <div
+        Object.entries(almFile.tags).map(([key, tag]) => (
+          <div key={tag.value} className="flex flex-row box-border m-0.5 h-6">
+            <Badge
+              title={`Filter by ${tag.label}`}
               key={tag.value}
-              className="flex flex-row box-border m-0.5 h-6 "
+              variant={tag.variant}
+              className="px-1 focus-visible:outline-none"
+              onClick={() =>
+                setFilterByTags((prevState) => [...prevState, tag])
+              }
             >
-              <Badge
-                title={`Filter by ${tag.label}`}
-                key={tag.value}
-                variant={tag.variant}
-                className="px-1 focus-visible:outline-none"
-                onClick={(e) => {
-                  setFilterByTags((prevState) => [...prevState, tag]);
-                }}
-              >
-                {tag.label}
-              </Badge>
-              <span
-                title={`Remove ${tag.label} tag`}
-                className={cn(
-                  badgeVariants({ variant: tag.variant }),
-                  "px-0 box-border border",
-                )}
-                onClick={() => removeTag(key)}
-              >
-                <Cross2Icon />
-              </span>
-            </div>
-          );
-        })}
+              {tag.label}
+            </Badge>
+            <span
+              title={`Remove ${tag.label} tag`}
+              className={cn(
+                badgeVariants({ variant: tag.variant }),
+                "px-0 box-border border",
+              )}
+              onClick={() => removeTag(key)}
+            >
+              <Cross2Icon />
+            </span>
+          </div>
+        ))}
+      {xmpKeywords.map((keyword, index) => (
+        <Badge key={index} variant="outline" className="px-1 m-0.5 h-6">
+          {keyword}
+        </Badge>
+      ))}
       <DropdownMenu>
         <DropdownMenuTrigger className="h-fit">
-           <div
-              className="flex flex-row box-border m-0.5 h-6 "
+          <div className="flex flex-row box-border m-0.5 h-6">
+            <Badge
+              title={t("add tag")}
+              variant="outline"
+              className="py-0.5 focus-visible:outline-none px-2 h-6"
             >
-          <Badge
-            title={t("add tag")}
-            variant="outline"
-            className="py-0.5 focus-visible:outline-none px-2 h-6"
-            >
-              
-            <PlusIcon />
+              <PlusIcon />
             </Badge>
-            </div>
+          </div>
         </DropdownMenuTrigger>
         <DropdownMenuContent>
           {availableTags &&
-            _.map(availableTags, (tag, key) => {
-              return (
-                <DropdownMenuItem
-                  onClick={() => writeTagToAlmFile({ [key]: tag })}
-                  key={key}
-                >
-                  {tag.label}
-                </DropdownMenuItem>
-              );
-            })}
-          {/*{availableTags?.map(tag => {*/}
-          {/*    return <DropdownMenuItem key={tag.label}>{tag.label}</DropdownMenuItem>*/}
-          {/*})}*/}
+            Object.entries(availableTags).map(([key, tag]) => (
+              <DropdownMenuItem
+                onClick={() => writeTagToAlmFile({ [key]: tag })}
+                key={key}
+              >
+                {tag.label}
+              </DropdownMenuItem>
+            ))}
           <AddTagDialogue setConfig={setConfig} config={config} />
         </DropdownMenuContent>
       </DropdownMenu>
