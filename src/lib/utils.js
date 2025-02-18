0af4dbb96282d034a45b5d640f4b5e79bdf7478b
Marcel <79279756+aofn@users.noreply.github.com>
2025-02-19 00:32:25 +0100

    feat: - reintroduce dark mode - add footer to sidebar with theme toggle

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,15 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getSystemTheme() {
+  if (typeof window !== "undefined") {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  }
+  return "light";
 }
